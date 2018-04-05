c2bbc2cd1ad39225fe73e4c56c0b0c1b567bc2ee
yakub.andi <yakub.andi@1rstwap.com>
2018-04-05 18:01:22 +0700

    initial commit

---
+++ src/test/java/com/smsapi/discovery/service/smsapidiscoveryservice/SmsapiDiscoveryServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.smsapi.discovery.service.smsapidiscoveryservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SmsapiDiscoveryServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
